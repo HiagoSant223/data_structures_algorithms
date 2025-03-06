4c54e676c29ac022df9183e164df38a13fc30bb5
Hiago Oliveira <hiagosantos223@gmail.com>
2025-03-06 17:58:20 -0300

    Manipulando variaveis e constantes

---
+++ variaveis.py
@@ -0,0 +1,32 @@
+# Variaveis com numeros inteiros(Int)
+numero = -3
+numero_jogos = 14
+numero_convidados = 15
+
+print(numero, numero_jogos, numero_convidados)
+
+# Variaveis com Ponto Flutuante(Float)
+pi = 3.14
+numero_euler = 2.71
+escala_terremoto = -4.55
+
+print(numero_euler, pi, escala_terremoto)
+
+# Variaveis do Tipo String e Char
+
+letra = 'a'
+palavra1 = "linguagem de programação"
+palavra2 = 'Python'
+
+print(palavra1, palavra2, letra)
+print("Concatenação de variaveis ", palavra1, palavra2, " e ", letra)
+
+idade = input("Digite sua idade: ")
+
+print("Sua idade é: ", idade)
+
+ph = input("Qual o ph do solo na ultima medição? ")
+print("O ph do solo é: ", ph)
+
+nome = input("Qual seu nome? ")
+print("Seu nome é: ", nome)
